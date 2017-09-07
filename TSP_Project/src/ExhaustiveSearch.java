84a4ef86d0703e41b94331ae6799e4a8f587d198
Tae <tkim@mines.edu>
2017-09-06 18:44:24 -0600

    fixed RunTest function to have inputs.

---
+++ TSP_Project/src/ExhaustiveSearch.java
@@ -0,0 +1,27 @@
+import java.awt.Point;
+import java.util.ArrayList;
+
+public class ExhaustiveSearch {
+
+	private ReadInput input;
+	private double totalDistance;
+	private ArrayList<Point> order;
+	
+	
+	public static void main(String[] args) {		
+			ExhaustiveSearch hello = new ExhaustiveSearch();
+			
+	}
+
+public ExhaustiveSearch(){
+	input = new ReadInput("Inputs.txt");
+	totalDistance = 0.0;
+	order = new ArrayList<Point>();	
+}
+
+public void RunTest(){
+	double distance = 999999999;
+	
+}
+
+}
--- TSP_Project/src/NearestNeighbor.java
+++ TSP_Project/src/NearestNeighbor.java
@@ -5,48 +5,45 @@ import java.util.ArrayList;
 
 //testing for git
 public class NearestNeighbor {
-	public ReadInput input;
-	public double totalDistance;
-	public ArrayList<Point> order;
+	private double totalDistance;
+	private ArrayList<Point> order;
 	
 	public static void main(String[] args) {		
 		NearestNeighbor hi = new NearestNeighbor();
-		hi.RunTest();
+		ReadInput input = new ReadInput("Inputs.txt");
+		hi.RunTest(input.points, input.visited, input.numPoints);
 	}
 	
 	NearestNeighbor(){
-		input = new ReadInput("Inputs.txt");
 		totalDistance = 0.0;
 		order = new ArrayList<Point>();
 	}
 
-	public void RunTest() {
-		order.add(input.points.get(0));
+	public void RunTest(ArrayList<Point> points, ArrayList<Boolean> visited, int n) {
+		order.add(points.get(0));
 		int i = 0;
-		while(input.visited.contains(false) && i < input.numPoints-1) { // while not all points are visited
-			order.add(FindNearestPoint(order.get(i)));
+		while(visited.contains(false) && i < n-1) { // while not all points are visited
+			
+			visited.set(points.indexOf(order.get(i)),true);//marks this point as visited
+			double distance = 999999999;
+			Point nearPoint = new Point();
+			for(int j = 0; j < n; j++) {
+				if(visited.get(j) == false) {					
+					double newDistance = Point2D.distance(order.get(i).getX(), order.get(i).getY(), points.get(j).getX(), points.get(j).getY());
+					if(newDistance < distance) {		
+						distance = newDistance;
+						nearPoint = points.get(j);
+					}
+				}
+			}
+			totalDistance += distance;
+			
+						
+			order.add(nearPoint);
 			i++;
 		} 
 		totalDistance += Point2D.distance(order.get(order.size()-1).getX(), order.get(order.size()-1).getY(), order.get(0).getX(), order.get(0).getY());
 		Print();
-	}
-	
-	
-	public Point FindNearestPoint(Point p) {
-		input.visited.set(input.points.indexOf(p),true);//marks this point as visited
-		double distance = 999999999;
-		Point nearPoint = new Point();
-		for(int i = 0; i < input.points.size(); i++) {
-			if(input.visited.get(i) == false) {					
-				double newDistance = Point2D.distance(p.getX(), p.getY(), input.points.get(i).getX(), input.points.get(i).getY());
-				if(newDistance < distance) {		
-					distance = newDistance;
-					nearPoint = input.points.get(i);
-				}
-			}
-		}
-		totalDistance += distance;
-		return nearPoint;
 	}
 	
 	public void Print() {
